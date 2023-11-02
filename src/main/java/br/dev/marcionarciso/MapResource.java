39a0640e97e9632949b813726f4388f5d8ca3d53
Márcio Narciso D. <marcionarciso@live.com>
2023-11-02 07:51:15 -0300

    Initial Commit. Lombok adicionado.

---
+++ src/main/java/br/dev/marcionarciso/MapResource.java
@@ -0,0 +1,16 @@
+package br.dev.marcionarciso;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class MapResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+}
---
+++ src/test/java/br/dev/marcionarciso/MapResourceIT.java
@@ -0,0 +1,8 @@
+package br.dev.marcionarciso;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MapResourceIT extends MapResourceTest {
+    // Execute the same tests but in packaged mode.
+}
